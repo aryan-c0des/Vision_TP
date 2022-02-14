a8624965fa0aef98be85934903d3b9c05c734953
Aryan Singh <aryansemail29@gmail.com>
2022-02-14 20:17:29 +0530

    Endless Runner

---
+++ UnityProjects/3DProjects/EndlessRunner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    bool alive = true;
+
+    public float speed = 5;
+    [SerializeField] Rigidbody rb;
+
+    float horizontalInput;
+    [SerializeField] float HorizontalMultiplier = 2;
+
+    public float speedIncreasePerPoint = 0.1f;
+
+    [SerializeField] float jumpForce = 400f;
+    [SerializeField] LayerMask groundMask;
+
+    private void FixedUpdate()
+    {
+        if (!alive) return;
+
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * HorizontalMultiplier;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if(Input.GetKeyDown(KeyCode.Space) && (transform.position.y >= 0.9 && transform.position.y <= 1))
+        {
+            Jump();
+        }
+
+        if (transform.position.y < -5)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        alive = false;
+        //restart the game
+        Invoke("Restart", 2);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Jump()
+    {
+        //check if we are currently grounded
+        float height = GetComponent<Collider>().bounds.size.y;
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, (height / 2) + 0.1f, groundMask);
+
+        //If we are, jump
+        rb.AddForce(Vector3.up * jumpForce);
+    }
+}
